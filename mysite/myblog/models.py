8abf99e96ef5c7d270bee470908f032c06ab3179
DonatasNoreika <donoras@gmail.com>
2025-10-31 09:35:13 +0200

    +Vieno įrašo puslapyje atvaizduojami komentarai

--- mysite/myblog/models.py
+++ mysite/myblog/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(to="Post", on_delete=models.CASCADE)
+    post = models.ForeignKey(to="Post", on_delete=models.CASCADE, related_name="comments")
     content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
