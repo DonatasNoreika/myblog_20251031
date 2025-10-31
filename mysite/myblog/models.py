1d948875aa47040726d7c681546990f1f3fea947
DonatasNoreika <donoras@gmail.com>
2025-10-31 08:38:05 +0200

    +Modelių aprašymas

--- mysite/myblog/models.py
+++ mysite/myblog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField()
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(to="Post", on_delete=models.CASCADE)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ALLOWED_HOSTS
 # Application definition
 
 INSTALLED_APPS = [
+    'myblog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
